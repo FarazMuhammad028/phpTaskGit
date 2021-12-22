5be11bc954615a1fbaa2ed987e5343524fe825bf
FarazMuhammad028 <50590150+FarazMuhammad028@users.noreply.github.com>
2021-12-22 10:49:05 +0500

    Add files via upload

--- FlexStart/Logout.php
+++ FlexStart/Logout.php
@@ -1,5 +1,9 @@
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <?php
 session_start();
 session_destroy();
-
-header("Location:index.html?msg=<div class='alert alert-success mt-lg-4'>Logged Out</div>");
+echo '<p></p>';
+echo '<script type="text/javascript">';
+echo 'swal("Logged Out");';
+echo '</script>';
+header("refresh:1;url=index.html");
